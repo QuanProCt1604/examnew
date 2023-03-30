142e1ace5cbaf6d35435d0ce9ecfbcf59492695f
QuanProCt1604 <127952803+QuanProCt1604@users.noreply.github.com>
2023-03-30 21:50:22 +0700

    Add files via upload

---
+++ NguyenHongQuan/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static NguyenHongQuan.IEnemy;
+
+namespace NguyenHongQuan
+{
+    internal class Enemy : IEnemy
+    {
+        IEnemy Id { get; set; }
+    }
+}
---
+++ NguyenHongQuan/IEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NguyenHongQuan
+
+{
+     public interface IEnemy
+    {
+        public enum ENEMY_TYPE
+        {
+            GOBLIN, BOSS, ORC
+        }
+        public enum ATTACK_TYPE
+        {
+            RANGE, MELEE
+        }
+            public long Id { get; set; }
+            public string Name { get; set; }
+            public ENEMY_TYPE Type { get; set; }
+
+            public int Health { get; set; }
+
+            public ATTACK_TYPE ATTACK { get; set; }
+            public float Damage { get; set; }
+        
+         void ShowInfo()
+        {
+            Console.WriteLine("Show Information: ");
+        }
+         
+    }
+    
+
+
+
+}
